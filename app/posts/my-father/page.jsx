b92b2bf678e5a19a6556f6cdc3eb4de34d69bb54
solovyovsdom <joelborofsky@mac.com>
2024-05-09 19:30:36 -0500

    chore: Add global styles to layout component

---
+++ app/posts/my-father/page.jsx
@@ -0,0 +1,10 @@
+export default function WatchMyFather() {
+  return (
+    <>
+      <h1>Thoughts on religion while my father dies</h1>
+      <p>
+        Filler text
+      </p>
+    </>
+  )
+}
---
+++ app/posts/second-post/page.jsx
@@ -0,0 +1,8 @@
+export default function SecondPost() {
+    return (
+        <>
+            <h1>Second Post</h1>
+            <p>This is the second post.</p>
+        </>
+    );
+}
